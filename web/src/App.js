9e8e3eef48cea3b1b076f98f9ad73b23b560ff17
Bryan Mey <bryan.j.mey@gmail.com>
2018-09-09 09:26:19 -0400

    load json data from server and add mock server for local development

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    data: {},
+  };
+
+  componentDidMount() {
+    const that = this;
+    fetch(`${process.env.REACT_APP_API}/data.json`)
+      .then(response => response.json())
+      .then(json => {
+        that.setState({ data: json });
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,6 +25,10 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Data:
+          {this.state.data && JSON.stringify(this.state.data)}
         </p>
       </div>
     );
