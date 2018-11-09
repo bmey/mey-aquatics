66152e0c88eb48fc957320bf0d910706e5a62b7a
Bryan Mey <bryan.j.mey@gmail.com>
2018-11-09 07:37:27 -0500

    sort lowest by all non-zero size prices. rename 'filter' to 'sort'

--- web/src/components/Filter/filter.js
+++ web/src/components/Filter/filter.js
@@ -1,13 +1,13 @@
 import { SORT_BY } from "./constants";
 
-const filter = (productList, sortType = SORT_BY.ALPHABETICAL) => {
+const sort = (productList, sortType = SORT_BY.ALPHABETICAL) => {
   if (!productList) {
     return [];
   }
 
   switch (sortType) {
     case SORT_BY.PRICE_LOW_TO_HIGH:
-      return productList.sort((a, b) => a.sizes["S"].price > b.sizes["S"].price);
+      return productList.sort((a, b) => getLowestPrice(a.sizes) > getLowestPrice(b.sizes));
     case SORT_BY.PRICE_HIGH_TO_LOW:
       return productList.sort((a, b) => getHighestPrice(a.sizes) < getHighestPrice(b.sizes));
     case SORT_BY.ALPHABETICAL:
@@ -20,10 +20,24 @@ const getHighestPrice = sizes => {
   let highest = 0;
   Object.keys(sizes).forEach(key => {
     const price = sizes[key].price;
-    highest = price > highest ? price : highest;
+    if (price > highest) {
+      highest = price;
+    }
   });
 
   return highest;
 };
 
-export default filter;
+const getLowestPrice = sizes => {
+  let lowest = Number.MAX_SAFE_INTEGER;
+  Object.keys(sizes).forEach(key => {
+    const price = sizes[key].price;
+    if (price < lowest && price > 0) {
+      lowest = price;
+    }
+  });
+
+  return lowest;
+};
+
+export default sort;
--- web/src/components/Filter/filter.test.js
+++ web/src/components/Filter/filter.test.js
@@ -1,36 +1,38 @@
-import filter from "./filter";
+import sort from "./filter";
 import { SORT_BY } from "./constants";
 
 describe("filter", () => {
   it("returns empty array when input is null", () => {
-    const result = filter(null);
+    const result = sort(null);
 
     expect(result).toEqual([]);
   });
 
   it("returns empty array when input is undefined", () => {
-    const result = filter(undefined);
+    const result = sort(undefined);
 
     expect(result).toEqual([]);
   });
 
   it("returns empty array when input is empty", () => {
-    const result = filter([]);
+    const result = sort([]);
 
     expect(result).toEqual([]);
   });
 
-  it("returns default sorting (alphabetical by common) when no sort specified", () => {
+  it("returns default sorting (alphabetical by common name) when no sort specified", () => {
     const productList = [setupProductItem("Z"), setupProductItem("A")];
-    const result = filter(productList);
+
+    const result = sort(productList);
 
     expect(result[0].id).toEqual("A");
     expect(result[1].id).toEqual("Z");
   });
 
-  it("sorts case-insensitive alphabetical by common when no sort specified", () => {
+  it("sorts case-insensitive alphabetical by common name when alphabetical specified", () => {
     const productList = [setupProductItem("aA"), setupProductItem("B"), setupProductItem("AA"), setupProductItem("aa")];
-    const result = filter(productList);
+
+    const result = sort(productList, SORT_BY.ALPHABETICAL);
 
     expect(result[0].id).toEqual("aA");
     expect(result[1].id).toEqual("AA");
@@ -40,7 +42,8 @@ describe("filter", () => {
 
   it("sorts by price (low to high) when low-to-high specified", () => {
     const productList = [setupProductItem("C", 10), setupProductItem("B", 15), setupProductItem("A", 10)];
-    const result = filter(productList, SORT_BY.PRICE_LOW_TO_HIGH);
+
+    const result = sort(productList, SORT_BY.PRICE_LOW_TO_HIGH);
 
     expect(result[0].id).toEqual("C");
     expect(result[1].id).toEqual("A");
@@ -49,24 +52,40 @@ describe("filter", () => {
 
   it("sorts by price (high to low) when high-to-low specified", () => {
     const productList = [setupProductItem("C", 10), setupProductItem("B", 15), setupProductItem("A", 10)];
-    const result = filter(productList, SORT_BY.PRICE_HIGH_TO_LOW);
+
+    const result = sort(productList, SORT_BY.PRICE_HIGH_TO_LOW);
 
     expect(result[0].id).toEqual("B");
     expect(result[1].id).toEqual("C");
     expect(result[2].id).toEqual("A");
   });
 
-  it("sorts by price (high to low) by highest non-zero price for each item", () => {
+  it("sorts by price (high to low) considering all prices an item lists for various sizes", () => {
     const productList = [
       setupProductItem("C", 0, { B: { price: 2 } }),
       setupProductItem("B", 0, { L: { price: 4 } }),
       setupProductItem("A", 1, { B: { price: 3 } }),
     ];
-    const result = filter(productList, SORT_BY.PRICE_HIGH_TO_LOW);
+
+    const result = sort(productList, SORT_BY.PRICE_HIGH_TO_LOW);
 
     expect(result[0].id).toEqual("B");
     expect(result[1].id).toEqual("A");
     expect(result[2].id).toEqual("C");
+  });
+
+  it("sorts by price (low to high) using lowest non-zero price for all sizes", () => {
+    const productList = [
+      setupProductItem("C", 0, { B: { price: 2 } }),
+      setupProductItem("B", 0, { L: { price: 4 } }),
+      setupProductItem("A", 5, { B: { price: 1 } }),
+    ];
+
+    const result = sort(productList, SORT_BY.PRICE_LOW_TO_HIGH);
+
+    expect(result[0].id).toEqual("A");
+    expect(result[1].id).toEqual("C");
+    expect(result[2].id).toEqual("B");
   });
 
   const setupProductItem = (name, price, sizeOptions = {}) => {
