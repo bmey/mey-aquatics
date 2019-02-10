de9bd0009974a45936005d50af3638e9e9bcd042
Bryan Mey <bryan.j.mey@gmail.com>
2019-02-10 09:21:23 -0500

    fix lint warning about multiline strong

--- web/src/components/ContactPage.jsx
+++ web/src/components/ContactPage.jsx
@@ -9,15 +9,14 @@ const email = "meysaquatics@gmail.com";
 const emailQuery = queryString.stringify({
   subject: "Mey's Aquatics Inquiry",
   body:
-    "(Please fill out the questions below)\n\n\
-My name is: \n\n\
-The best way to contact me is: \n\n\
-I am looking to: \n\
-- Buy\n\
-- Trade\n\
-- Question about an item\n\
-- Something else\n\
-  ",
+    "(Please fill out the questions below)\n\n" +
+    "My name is: \n\n" +
+    "The best way to contact me is: \n\n" +
+    "I am looking to: \n" +
+    "- Buy\n" +
+    "- Trade\n" +
+    "- Question about an item\n" +
+    "- Something else\n",
 });
 
 const ContactPage = () => (
