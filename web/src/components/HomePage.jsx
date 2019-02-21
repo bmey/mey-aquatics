e9180911419f87ff14b510165a3f7af9ad33b32a
Bryan Mey <bryan.j.mey@gmail.com>
2019-02-21 07:09:23 -0500

    add primary button to homepage

--- web/src/components/HomePage.jsx
+++ web/src/components/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
+import "./HomePage.css";
 
 const Home = () => (
   <div data-cy="homepage">
-    <header className="App-header">
+    <header className="homepage-header">
       <div>
         <h1 className="App-title">Welcome to Mey's Aquatics!</h1>
-        <div className="text-center">
+        <div className="d-flex flex-column align-items-center">
           <em>Caring for fish since 1970</em>
+          <Link to="/livestock" style={{ marginTop: "40px" }}>
+            <Button color="primary">Browse</Button>
+          </Link>
         </div>
       </div>
     </header>
