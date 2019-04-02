f30e9dbd3c81f65ad30783d06ce8d18eb7e988ac
Bryan Mey <bryan.j.mey@gmail.com>
2019-04-02 09:44:58 -0400

    push fixed width prop down to image component

--- web/src/components/Image/Image.jsx
+++ web/src/components/Image/Image.jsx
@@ -3,9 +3,9 @@ import { Image as CloudinaryImage } from 'cloudinary-react';
 
 const Image = props => {
   if (process.env.NODE_ENV === 'production' || /true/i.test(process.env.REACT_APP_USE_CLOUDINARY)) {
-    return <CloudinaryImage {...props} />;
+    return <CloudinaryImage {...props} width='500' />;
   } else {
-    return <img src={`${window.location.origin}/fish.webp`} alt='' />;
+    return <img src={`${window.location.origin}/fish.webp`} alt='' width='500' />;
   }
 };
 
