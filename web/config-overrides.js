8def13fa427585365fd2c21b1b266cc57883a547
Bryan Mey <bryan.j.mey@gmail.com>
2019-09-21 17:32:46 -0400

    load axios from cdn

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -2,6 +2,7 @@ module.exports = function override(config, env) {
   return {
     ...config,
     externals: {
+      axios: 'axios',
       lodash: '_',
       react: 'React',
       'react-dom': 'ReactDOM',
