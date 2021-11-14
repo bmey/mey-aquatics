4748b74cd6f14a37acd29387b669b514095d392f
Bryan Mey <bryan.j.mey@gmail.com>
2021-11-13 23:32:44 -0500

    add watermark

--- web/src/components/Image/Image.jsx
+++ web/src/components/Image/Image.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Image as CloudinaryImage } from 'cloudinary-react';
+import { Image as CloudinaryImage, Transformation } from 'cloudinary-react';
 
 const Image = props => {
   if (process.env.NODE_ENV === 'production' || /true/i.test(process.env.REACT_APP_USE_CLOUDINARY)) {
-    return <CloudinaryImage {...props} width='500' />;
+    return (
+      <CloudinaryImage {...props} width='500'>
+        <Transformation overlay={{ fontFamily: "Pacifico", fontSize: 150, text: "Mey's Aquatics" }} color="#414542" gravity="south_east" x="50" />
+      </CloudinaryImage>
+    );
   } else {
     return <img src={`${window.location.origin}/fish.webp`} alt='' width='500' />;
   }
