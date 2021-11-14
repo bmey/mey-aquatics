29581ef6be004b9ff5c51badbbb117fcd690f27b
Bryan Mey <bryan.j.mey@gmail.com>
2021-11-13 23:40:53 -0500

    revert watermark for now. buggy bc not all pictures are same size/ratio

--- web/src/components/Image/Image.jsx
+++ web/src/components/Image/Image.jsx
@@ -5,7 +5,7 @@ const Image = props => {
   if (process.env.NODE_ENV === 'production' || /true/i.test(process.env.REACT_APP_USE_CLOUDINARY)) {
     return (
       <CloudinaryImage {...props} width='500'>
-        <Transformation overlay={{ fontFamily: "Pacifico", fontSize: 150, text: "Mey's Aquatics" }} color="#414542" gravity="south_east" x="50" />
+        {/* <Transformation overlay={{ fontFamily: "Pacifico", fontSize: 150, text: "Mey's Aquatics" }} color="#414542" gravity="south_east" x="50" /> */}
       </CloudinaryImage>
     );
   } else {
