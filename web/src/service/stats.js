b831e316138169c65c8ffeb216706e4cbba87a1d
Bryan Mey <bryan.j.mey@gmail.com>
2024-02-18 16:15:50 -0500

    filter out-of-stock from stat

--- web/src/service/stats.js
+++ web/src/service/stats.js
@@ -1,6 +1,13 @@
+import filter from './filter';
+import { FILTER } from '../utility/constants';
+
 export const countEndangeredSpecies = fish => {
   if (fish === null || !Array.isArray(fish)) {
     return 0;
   }
-  return fish.filter(item => item.onCaresList).length;
+
+  const inStockFilter = [{ type: FILTER.IN_STOCK }];
+  const inStock = filter(fish, inStockFilter);
+
+  return inStock.filter(item => item.onCaresList).length;
 };
--- web/src/service/stats.test.js
+++ web/src/service/stats.test.js
@@ -21,31 +21,42 @@ describe('stats', () => {
     });
 
     it('returns zero when no endangered fish', () => {
-      const result = subject.countEndangeredSpecies([{ onCaresList: false }]);
+      const result = subject.countEndangeredSpecies([{ onCaresList: false, sizes: { B: { count: 1 } } }]);
 
       expect(result).toBe(0);
     });
 
     it('returns zero when fish have undefined property', () => {
-      const result = subject.countEndangeredSpecies([{ id: 'test' }]);
+      const result = subject.countEndangeredSpecies([{ id: 'test', sizes: { B: { count: 1 } } }]);
 
       expect(result).toBe(0);
     });
 
     it('returns one when one endangered fish', () => {
-      const result = subject.countEndangeredSpecies([{ onCaresList: true }]);
+      const result = subject.countEndangeredSpecies([{ onCaresList: true, sizes: { B: { count: 1 } } }]);
 
       expect(result).toBe(1);
     });
 
     it('returns two when two endangered fish', () => {
       const result = subject.countEndangeredSpecies([
-        { onCaresList: true },
-        { onCaresList: true },
-        { onCaresList: false },
+        { onCaresList: true, sizes: { B: { count: 1 } } },
+        { onCaresList: true, sizes: { B: { count: 1 } } },
+        { onCaresList: false, sizes: { B: { count: 1 } } },
       ]);
 
       expect(result).toBe(2);
+    });
+
+    it('returns only in-stock items', () => {
+      const productList = [
+        { id: 'out-of-stock', onCaresList: true, sizes: { B: { count: 0 } } },
+        { id: 'in-stock', onCaresList: true, sizes: { B: { count: 1 } } },
+      ];
+      
+      const result = subject.countEndangeredSpecies(productList);
+  
+      expect(result).toBe(1);
     });
   });
 });
